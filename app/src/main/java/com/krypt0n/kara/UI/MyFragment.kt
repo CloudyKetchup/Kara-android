b13003f512b16834fa9c05f79757e6ae25827a0b
krypt0n <maxdodon25@gmail.com>
2019-01-26 20:56:04 +0200

    development

--- app/src/main/java/com/krypt0n/kara/UI/MyFragment.kt
+++ app/src/main/java/com/krypt0n/kara/UI/MyFragment.kt
@@ -1,13 +1,20 @@
 package com.krypt0n.kara
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class NotesFragment : Fragment() {
+class MyFragment() : Fragment() {
+    private var layout : Int = 0
+
+    @SuppressLint("ValidFragment")
+    constructor(l : Int) : this() {
+        layout = l
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.notes_layout,container,false)
+        return inflater.inflate(layout,container,false)
     }
 }
